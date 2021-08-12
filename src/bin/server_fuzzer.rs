ab247ceb35eb06a6f230b1bea5980fefeb6d60d3
Daniil Naumetc <daniil.naumetc@gmail.com>
2021-08-12 20:17:30 +0200

    kek

--- src/bin/server_fuzzer.rs
+++ src/bin/server_fuzzer.rs
@@ -1,31 +1,52 @@
 use case::fuzzing::gen_json;
 use hyper::{Body, Method, Request, Uri, Client};
+use std::thread;
+
+use tokio::runtime::Runtime;
+use tokio::time::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // This is where we will setup our HTTP client requests.
 
-    println!("type,client,tx,amount");
+    let mut children = vec![];
 
-    let client = Client::new();
+    for t_i in 0..5 {
+        // Spin up another thread
+        children.push(thread::spawn(move || {
+            let mut rt = Runtime::new().unwrap();
 
-    // Parse an `http::Uri`...
-    // let uri = "http://127.0.0.1:3030/hello".parse()?;
+            rt.block_on(async move {
+                let client = Client::new();
+
+                // Parse an `http::Uri`...
+                // let uri = "http://127.0.0.1:3030/hello".parse()?;
+
+                
+
+                let mut rng = rand::thread_rng();
+                for i in 0..30000000_u64 {
+                    // Await the response...
+                    let req = Request::builder()
+                        .method(Method::POST)
+                        .uri("http://127.0.0.1:3030/hello")
+                        .header("content-type", "application/json")
+                        .body(Body::from(gen_json(&mut rng))).unwrap();
+                    let resp = client.request(req).await.unwrap();
+
+                    println!("{}-{} Response: {}", t_i, i, resp.status());
+                }
+            });
+            
+        }));
+    }
+
+    for child in children {
+        // Wait for the thread to finish. Returns a result.
+        let _ = child.join();
+    }
 
     
-
-    let mut rng = rand::thread_rng();
-    for i in 0..30000000_u64 {
-        // Await the response...
-        let req = Request::builder()
-            .method(Method::POST)
-            .uri("http://127.0.0.1:3030/hello")
-            .header("content-type", "application/json")
-            .body(Body::from(gen_json(&mut rng)))?;
-        let resp = client.request(req).await?;
-
-        println!("{} Response: {}", i, resp.status());
-    }
 
     Ok(())
 }
@@ -33,3 +54,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 // fn main() {
     
 // }
+
+// 2400/s
