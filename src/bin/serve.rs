80e9ad4289f11a2a5e3e98d8daedb00266da38b1
Daniil Naumetc <daniil.naumetc@gmail.com>
2021-08-15 22:41:22 +0200

    working on the cli

--- src/bin/serve.rs
+++ src/bin/serve.rs
@@ -30,7 +30,7 @@ impl Router {
 
     pub fn process(&self, t: Transaction) -> Result<(), DBError> {
         let mut db = self.db.lock().unwrap();
-            // println!("accounts: {}", db.describe_accounts());
+            println!("accounts: {}", db.describe_accounts());
             db.process_new_transaction(t)
     }
 }
@@ -38,75 +38,15 @@ impl Router {
 #[tokio::main]
 async fn main() {
 
-    
-
-    // let mut db = Db::new();
-    // let r = Router::new(3);
-    
-    // // let f = File::open("transactions.csv").unwrap();
-    // // let reader = BufReader::new(f);
-    // // let mut rdr = csv::Reader::from_reader(reader);
-    // let mut rdr = csv::Reader::from_reader(io::stdin());
-    
-
-    // let verbose = false;
-    
-    
-    // for result in rdr.deserialize::<Transaction>() {
-    //     match result {
-    //         Ok(record) => {
-    //             if verbose {println!("{:?}", record)}
-    //             if let Some(e) = db.process_new_transaction(record) {
-    //                 if verbose {println!("E: {:?}", e)}
-    //             }
-    //         },
-    //         Err(e) => {
-    //             if verbose {println!("E: {:?}", e)}
-    //         },
-    //     }
-    // }
-
-    // println!("\n\nAccounts:\n{}", db.describe_accounts());
-
-
-
-
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    // let hello = warp::path!("hello" / String)
-    //     .map(|name| format!("Hello, {}!", name));
-
-    // warp::serve(hello)
-    //     .run(([127, 0, 0, 1], 3030))
-    //     .await;
-
-
-
-
-
-    // let hello = warp::path!("hello")
-    //     .and(warp::body::content_length_limit(1024 * 32))
-    //     .and(warp::body::json())
-    //     .map(|simple_map: std::collections::HashMap<String, String>| {
-    //         println!("{:?}", simple_map);
-    //         "Got a JSON body!"
-    //     });
-    //     // .map(|name| format!("Hello, {}!", name));
-
-    // warp::serve(hello)
-    //     .run(([127, 0, 0, 1], 3030))
-    //     .await;
     let router = Router::new();
-    // let router = Arc::new(Router::new(3));
-
-
 
     let with_state = warp::any().map(move || router.clone());
 
-    let debug = false;
+    let debug = true;
 
 
 
-    let hello = warp::path!("hello")
+    let hello = warp::any()
         .and(warp::body::content_length_limit(1024 * 32))
         .and(warp::body::json())
         .and(with_state)
@@ -122,9 +62,7 @@ async fn main() {
             }
 
             "Got a JSON body!"
-            // warp::reply()
         });
-        // .map(|name| format!("Hello, {}!", name));
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 3030))
